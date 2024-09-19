8418f0aa6963078d7e4435d90bf3fcb8349afa25
21week <dlgkswn0711@naver.com>
2024-09-19 22:01:01 +0900

    loop

---
+++ ex08-99-homework.c
@@ -0,0 +1,24 @@
+/* 숙제
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    for(int i = 0; i < 7; i++) {
+
+        for(int j = 0; j<i+1; j++) {
+        printf("*");
+        }
+
+        printf("\n");
+
+    }
+
+
+
+
+    return 0;
+}
